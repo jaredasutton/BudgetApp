5dbc916efa70441b07bd56def3ba9ec9de2e2f3e
Eric Luu <ericluu24@gmail.com>
2019-08-20 17:20:06 -0400

    Added button when clicked to show input for each category

--- client/src/SpecificBudgetPage.js
+++ client/src/SpecificBudgetPage.js
@@ -3,13 +3,44 @@ import React from "react";
 class SpecificBudgetPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      budgetLineInput: "",
+      categories: [
+        "Housing",
+        "Transportation",
+        "Food",
+        "Entertainment",
+        "Savings",
+        "Misc"
+      ]
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(string) {
+    this.setState({ budgetLineInput: string });
+  }
+
   render() {
     return (
       <div>
         <h1>Budget #1</h1>
-        <button onClick={this.props.handleMainClick}>Main</button>
+
+        {this.state.categories.map(item => {
+          return (
+            <div key={item}>
+              <h2>{item}</h2>
+              {this.state.budgetLineInput === item ? <input /> : null}
+              <button
+                onClick={() => {
+                  this.handleClick(item);
+                }}
+              >
+                +
+              </button>
+            </div>
+          );
+        })}
       </div>
     );
   }
