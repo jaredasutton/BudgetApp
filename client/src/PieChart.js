100d1d32e7dbc3527e89e6375cd0f9f5feabd40d
Eric Luu <ericluu24@gmail.com>
2019-08-21 15:47:54 -0400

    Implemented pieChart to SpecificBudgetPage

--- client/src/PieChart.js
+++ client/src/PieChart.js
@@ -1,12 +1,49 @@
 import React from "react";
+// import * as d3 from "d3";
+import { PieChart } from "react-d3-components";
 
-class PieChart extends React.Component {
+class PieChartContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: { values: [] }
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  componentDidMount() {
+    this.setState({
+      data: {
+        values: [
+          { x: "SomethingA", y: 2 },
+          { x: "SomethingB", y: 5 },
+          { x: "SomethingC", y: 3 }
+        ]
+      }
+    });
+  }
+
+  handleClick() {
+    const chartValue = this.state.data.values.slice();
+    chartValue.push({ x: "placeHolder", y: 2 });
+    this.setState({ data: { values: chartValue } });
+  }
+
   render() {
-    return null;
+    var sort = null;
+    return (
+      <div>
+        <PieChart
+          data={this.state.data}
+          width={600}
+          height={400}
+          margin={{ top: 10, bottom: 10, left: 100, right: 100 }}
+          sort={sort}
+        />
+        <button onClick={this.handleClick}>Add</button>
+      </div>
+    );
   }
 }
 
-export default PieChart;
+export default PieChartContainer;
