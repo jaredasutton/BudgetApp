08257bbc824a8f7a431d962630fc57a23bd294ae
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-08-22 13:51:53 -0400

    Re-implement categoryListItem as functional component.

--- client/src/BudgetLine.js
+++ client/src/BudgetLine.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CreateSpendSaveForm from "./CreateSpendSaveForm.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import ListItem from "@material-ui/core/ListItem";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary
+  }
+}));
 
 let BudgetLine = ({
   budgetLine,
@@ -8,39 +22,42 @@ let BudgetLine = ({
   spendSaves,
   paymentAccounts
 }) => {
+  const classes = useStyles();
   const [showCreateSS, setShowCreateSS] = React.useState(false);
   const [createSSXY, setCreateSSXY] = React.useState(["0px", "0px"]);
   return (
-    <div className="budget-line">
-      {budgetLine.name}: ${budgetLine.expected_spending}{" "}
-      <small>
-        {spendSaves
-          .map(spendSave => {
-            return `$${spendSave.amount} for ${spendSave.name}`;
-          })
-          .join(", ")}
-        {" - "}
-        {
-          <a
-            href="#"
-            onClick={e => {
-              setShowCreateSS(true);
-              setCreateSSXY([e.clientX + "px", e.clientY + "px"]);
-            }}
-          >
-            Add New Spending
-          </a>
-        }
-      </small>
-      {showCreateSS && (
-        <CreateSpendSaveForm
-          postNewSpendSave={postNewSpendSave}
-          paymentAccounts={paymentAccounts}
-          style={{ top: createSSXY[1], left: createSSXY[0] }}
-          exit={() => setShowCreateSS(false)}
-        />
-      )}
-    </div>
+    <ListItem className={classes.root}>
+      <Paper className={classes.paper}>
+        {budgetLine.name}: ${budgetLine.expected_spending}{" "}
+        <small>
+          {spendSaves
+            .map(spendSave => {
+              return `$${spendSave.amount} for ${spendSave.name}`;
+            })
+            .join(", ")}
+          {" - "}
+          {
+            <a
+              href="#"
+              onClick={e => {
+                setShowCreateSS(true);
+                setCreateSSXY([e.clientX + "px", e.clientY + "px"]);
+              }}
+            >
+              Add New Spending
+            </a>
+          }
+        </small>
+        {showCreateSS && (
+          <CreateSpendSaveForm
+            postNewSpendSave={postNewSpendSave}
+            paymentAccounts={paymentAccounts}
+            style={{ top: createSSXY[1], left: createSSXY[0] }}
+            exit={() => setShowCreateSS(false)}
+          />
+        )}
+      </Paper>
+    </ListItem>
   );
 };
 
--- client/src/CategoryListForm.js
+++ client/src/CategoryListForm.js
@@ -1,31 +1,52 @@
 import React from "react";
 import CategoryListItem from "./CategoryListItem.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
 
 const CategoryListForm = props => {
+  const classes = useStyles();
   return (
-    <div>
-      {props.categories.map(item => {
-        return (
-          <CategoryListItem
-            key={item}
-            item={item}
-            budgetLineInput={props.budgetLineInput}
-            categories={props.categories}
-            handleClick={props.handleClick}
-            postNewBudgetLine={props.postNewBudgetLine}
-            budgetLines={props.budgetLines[item]}
-            totalSpending={props.budgetLines[item].reduce(
-              (acc, curr) => acc + curr.expected_spending,
-              0
-            )}
-            postNewSpendSave={(ssObj, subcat) =>
-              props.postNewSpendSave(ssObj, subcat, item)
-            }
-            spendSaves={props.spendSaves[item] || {}}
-            paymentAccounts={props.paymentAccounts}
-          />
-        );
-      })}
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            {props.categories.map(item => {
+              return (
+                <CategoryListItem
+                  key={item}
+                  item={item}
+                  budgetLineInput={props.budgetLineInput}
+                  categories={props.categories}
+                  handleClick={props.handleClick}
+                  postNewBudgetLine={props.postNewBudgetLine}
+                  budgetLines={props.budgetLines[item]}
+                  totalSpending={props.budgetLines[item].reduce(
+                    (acc, curr) => acc + curr.expected_spending,
+                    0
+                  )}
+                  postNewSpendSave={(ssObj, subcat) =>
+                    props.postNewSpendSave(ssObj, subcat, item)
+                  }
+                  spendSaves={props.spendSaves[item] || {}}
+                  paymentAccounts={props.paymentAccounts}
+                />
+              );
+            })}
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
