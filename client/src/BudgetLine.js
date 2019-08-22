a035540294c2194a964b4b920e926b862c79b74f
Eric Luu <ericluu24@gmail.com>
2019-08-22 14:41:15 -0400

    Centered budgetLines

--- client/src/BudgetLine.js
+++ client/src/BudgetLine.js
@@ -4,14 +4,24 @@ import CreateSpendSaveForm from "./CreateSpendSaveForm.js";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import ListItem from "@material-ui/core/ListItem";
+// import ListItem from "@material-ui/core/ListItem";
 
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     color: theme.palette.text.secondary
+//   }
+// }));
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
+    textAlign: "center",
     color: theme.palette.text.secondary
   }
 }));
@@ -25,39 +35,44 @@ let BudgetLine = ({
   const classes = useStyles();
   const [showCreateSS, setShowCreateSS] = React.useState(false);
   const [createSSXY, setCreateSSXY] = React.useState(["0px", "0px"]);
+
   return (
-    <ListItem className={classes.root}>
-      <Paper className={classes.paper}>
-        {budgetLine.name}: ${budgetLine.expected_spending}{" "}
-        <small>
-          {spendSaves
-            .map(spendSave => {
-              return `$${spendSave.amount} for ${spendSave.name}`;
-            })
-            .join(", ")}
-          {" - "}
-          {
-            <a
-              href="#"
-              onClick={e => {
-                setShowCreateSS(true);
-                setCreateSSXY([e.clientX + "px", e.clientY + "px"]);
-              }}
-            >
-              Add New Spending
-            </a>
-          }
-        </small>
-        {showCreateSS && (
-          <CreateSpendSaveForm
-            postNewSpendSave={postNewSpendSave}
-            paymentAccounts={paymentAccounts}
-            style={{ top: createSSXY[1], left: createSSXY[0] }}
-            exit={() => setShowCreateSS(false)}
-          />
-        )}
-      </Paper>
-    </ListItem>
+    <Grid container spacing={3} container justify="center">
+      {/* <ListItem className={classes.root}> */}
+      <Grid item xs={10}>
+        <Paper className={classes.paper}>
+          {budgetLine.name}: ${budgetLine.expected_spending}{" "}
+          <small>
+            {spendSaves
+              .map(spendSave => {
+                return `$${spendSave.amount} for ${spendSave.name}`;
+              })
+              .join(", ")}
+            {" - "}
+            {
+              <a
+                href="#"
+                onClick={e => {
+                  setShowCreateSS(true);
+                  setCreateSSXY([e.clientX + "px", e.clientY + "px"]);
+                }}
+              >
+                Add New Spending
+              </a>
+            }
+          </small>
+          {showCreateSS && (
+            <CreateSpendSaveForm
+              postNewSpendSave={postNewSpendSave}
+              paymentAccounts={paymentAccounts}
+              style={{ top: createSSXY[1], left: createSSXY[0] }}
+              exit={() => setShowCreateSS(false)}
+            />
+          )}
+        </Paper>
+        {/* </ListItem> */}
+      </Grid>
+    </Grid>
   );
 };
 
