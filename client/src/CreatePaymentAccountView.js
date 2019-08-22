59c3b25499329a107bfdfa47bb75b3140e26dcc8
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-08-22 15:33:02 -0400

    Materialize payment account input.

--- client/src/CreatePaymentAccountView.js
+++ client/src/CreatePaymentAccountView.js
@@ -1,73 +1,91 @@
 import React from "react";
+import CreatePaymentAccountForm from "./CreatePaymentAccountForm.js";
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-class CreatePaymentAccountView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      accType: "Debit/Checking",
-      newPayment: false
-    };
-    this.handleChange = this.handleChange.bind(this);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
   }
+}));
 
-  handleChange(key, value) {
-    let newStatePiece = {};
-    newStatePiece[key] = value;
-    this.setState(newStatePiece);
-  }
+let CreatePaymentAccountView = props => {
+  let [name, setName] = React.useState("");
+  let [accType, setAccType] = React.useState("Debit/Checking");
+  let [newPayment, setNewPayment] = React.useState(false);
+  const classes = useStyles();
 
-  render() {
-    return (
-      <div>
-        <div>
-          {this.props.paymentAccounts.map((item, i) => {
-            return (
-              <p key={item.name + i}>
-                {item.name} - {item.acc_type}
-              </p>
-            );
-          })}
-          {this.state.newPayment ? (
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                onChange={e => this.handleChange("name", e.target.value)}
-                name="name"
-                value={this.state.name}
-              />{" "}
-              <label htmlFor="accType" />
-              <select
-                onChange={e => this.handleChange("accType", e.target.value)}
-                name="accType"
-                value={this.state.accType}
-              >
-                <option value="Debit/Checking">Debit/Checking</option>
-                <option value="Credit">Credit</option>
-              </select>
-              <button
-                onClick={() =>
-                  this.props.postNewPaymentAccount({
-                    name: this.state.name,
-                    accType: this.state.accType
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <div>
+            {props.paymentAccounts.map((item, i) => {
+              return (
+                <p key={item.name + i}>
+                  <strong>{item.name}</strong> -{" "}
+                  {["Debit/Checking", "Credit"][item.acc_type - 1]}
+                </p>
+              );
+            })}
+            {newPayment ? (
+              // <div>
+              //   <label htmlFor="name">Name</label>
+              //   <input
+              //     onChange={e => setName(e.target.value)}
+              //     name="name"
+              //     value={name}
+              //   />{" "}
+              //   <label htmlFor="accType" />
+              //   <select
+              //     onChange={e => setAccType(e.target.value)}
+              //     name="accType"
+              //     value={accType}
+              //   >
+              //     <option value="Debit/Checking">Debit/Checking</option>
+              //     <option value="Credit">Credit</option>
+              //   </select>
+              //   <button
+              //     onClick={() =>
+              //       props.postNewPaymentAccount({
+              //         name: name,
+              //         accType: accType
+              //       })
+              //     }
+              //   >
+              //     Submit
+              //   </button>
+              // </div>
+              <CreatePaymentAccountForm
+                name={name}
+                accType={accType}
+                updateName={setName}
+                updateAccType={setAccType}
+                handleSubmit={() =>
+                  props.postNewPaymentAccount({
+                    name: name,
+                    accType: accType
                   })
                 }
-              >
-                Submit
-              </button>
-            </div>
-          ) : null}
-          <button
-            onClick={() => {
-              this.setState({ newPayment: true });
-            }}
-          >
-            +
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+              />
+            ) : null}
+            <button
+              onClick={() => {
+                setNewPayment(true);
+              }}
+            >
+              +
+            </button>
+          </div>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default CreatePaymentAccountView;
